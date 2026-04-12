71e3d665b8902832fc16375adb47c930f1d2a8f5
Anton Antonov <anton.antonov@rwth-aachen.de>
2026-04-12 19:19:13 +0200

    - Add PTree normalization. - BIM implemented.

--- inductive_miner/main.py
+++ inductive_miner/main.py
@@ -23,6 +23,7 @@ from inductive_miner.im_utils import (
     assert_rules_supported,
     base_cases,
     intersection_of_logs,
+    normalize_tree,
     repair_behavior,
 )
 from pm4py.objects.process_tree.obj import Operator, ProcessTree
@@ -63,6 +64,163 @@ def handle_empty_traces(log, im_function, rules: List[AbstractRule] = None):
 
 def preprocess_log(log, activity_key="concept:name", case_key="case:concept:name"):
     return log.groupby(case_key)[activity_key].apply(list).tolist()
+
+
+def apply_BIM_with_rules(
+    log: Union[pd.DataFrame, List],
+    rules: List[AbstractRule] = [],
+    activity_key="concept:name",
+    case_key="case:concept:name",
+):
+    if isinstance(log, pd.DataFrame):
+        log = preprocess_log(log, activity_key=activity_key, case_key=case_key)
+
+    act_in_log = set([e for trace in log for e in trace])
+
+    if rules:
+        intersection_logs = [r.apply(log) for r in rules]
+        intersection_logs = intersection_of_logs(intersection_logs)
+        if len(intersection_logs) == 0:
+            act_in_log = set([e for trace in log for e in trace])
+
+            raise Exception(
+                f"The support of the rules {rules} is 0. Activities are {act_in_log}."
+            )
+    process_tree = ProcessTree()
+
+    dfg = DirectlyFollowsGraph(log)
+    dfg_graph = dfg.graph
+
+    # Try to apply the cuts in order of precedence
+    cut_classes = [
+        BinaryExclusiveChoiceCut,
+        BinaryStrictSequenceCut,
+        BinaryConcurrentCut,
+        BinaryLoopCut,
+    ]
+    ops = [Operator.XOR, Operator.SEQUENCE, Operator.PARALLEL, Operator.LOOP]
+    # Check if the log has exactly one activity or empty traces
+    empty_traces = handle_empty_traces(log, apply_IM_with_rules, rules=rules)
+    # print(f"Log is: {log}")
+    if empty_traces is not None:
+        return empty_traces
+
+    if len(dfg_graph.nodes) <= 1:
+        # print(f"Applying base case to log {log}")
+        process_tree = base_cases(
+            log, process_tree, dfg_graph, apply_IM_with_rules, rules
+        )
+        if ENABLE_PRINTS:
+            print(f"BASE CASE TREE {process_tree}")
+
+        return process_tree
+    log_act_set = set([act for trace in log for act in trace])
+    for cut_class, op in zip(cut_classes, ops):
+        cut = cut_class(dfg_graph)
+        groups = cut.discover()
+        if ENABLE_PRINTS:
+            print(f"Cut {op} discovered with groups {groups}")
+        if groups is not None:
+            if ENABLE_PRINTS:
+                print(f"Groups are: {groups}")
+            unsat_rules = cut.check_rules(rules, groups)
+            if unsat_rules:
+
+                repaired_log = repair_behavior(
+                    log, unsat_rules, apply_BIM_with_rules, rules
+                )
+                if ENABLE_PRINTS:
+                    print(
+                        f"Unsat rules for {op} with groups {groups}: {[str(r) for r in unsat_rules]}, Repaired via: {repaired_log}"
+                    )
+
+                if repaired_log:
+                    return repaired_log
+                else:
+                    continue
+
+            process_tree = ProcessTree(operator=op)
+            sublogs = cut.project(
+                log, groups, activity_key=activity_key, case_key=case_key
+            )
+            projected_rules = cut.project_rules(rules, groups)
+            for i in range(len(sublogs)):
+                assert_rules_supported(
+                    "In rule refinement:", sublogs[i], projected_rules[i]
+                )
+
+                child_node = apply_BIM_with_rules(
+                    sublogs[i],
+                    projected_rules[i],
+                    activity_key=activity_key,
+                    case_key=case_key,
+                )
+                add_child(process_tree, child_node)
+            if ENABLE_PRINTS:
+                print("***")
+                print("ACTS:", sorted(log_act_set))
+                print("RULES:", [str(r) for r in rules])
+                print("OP:", op)
+                print("GROUPS:", [sorted(g) for g in groups])
+                print(
+                    "PROJECTED RULES:", [proj_group for proj_group in projected_rules]
+                )
+                print(
+                    "SUBLOGS:",
+                    [
+                        {
+                            "acts": sorted(set(a for t in sl for a in t)),
+                            "n_traces": len(sl),
+                            "n_empty": sum(1 for t in sl if len(t) == 0),
+                        }
+                        for sl in sublogs
+                    ],
+                )
+                print("SOURCE:", "cut")
+                print("***")
+            return process_tree
+    start_activities = dfg.start_activities
+    end_activities = dfg.end_activities
+    order_of_fall_throughs = [
+        empty,
+        activity_once,
+        activity_concur,
+        s_tau,
+        n_tau,
+        flower_model,
+        rule_seq,
+    ]
+    name_of_fall_throughs = [
+        "empty",
+        "once",
+        "concur",
+        "s_tau",
+        "tau",
+        "flower",
+        "rseq",
+    ]
+    for idx, fallthrough in enumerate(order_of_fall_throughs):
+        # print(f"Trying to apply: {name_of_fall_throughs[idx]}")
+        res = fallthrough(
+            dfg=dfg.graph,
+            start_activities=start_activities,
+            end_activities=end_activities,
+            log=log,
+            cut_order=cut_classes,
+            im_function=apply_BIM_with_rules,
+            rules=rules,
+        )
+        if res:
+            if ENABLE_PRINTS:
+                print("---")
+                print("APPLIED FALLTHROUGH:", name_of_fall_throughs[idx])
+                print("result", res)
+
+                print("---")
+
+            res.parent = process_tree.parent
+            return res
+    return ProcessTree()
 
 
 def apply_IM_with_rules(
@@ -429,17 +587,26 @@ if __name__ == "__main__":
         ResponseRule(["A_Denied", "W_Complete application"]),
     ]
     # rules = [ExistenceRule(["A_FINALIZED"])]
-    # rules = [AtMostOnceRule(["O_Create Offer"]), PrecedenceRule(["O_Accepted", "A_Pending"])]
+    rules = [
+        AtMostOnceRule(["O_Create Offer"]),
+        PrecedenceRule(["O_Accepted", "A_Pending"]),
+    ]
     # rules = [NotSuccessionRule(["O_Create Offer", "W_Call after offers"])]
-    model = apply_IM_with_rules(bpic_log, rules)
+    model = normalize_tree(apply_BIM_with_rules(bpic_log, rules))
     net, im, fm = pm4py.convert_to_petri_net(model)
+    net, im, fm = pm4py.reduce_petri_net_implicit_places(net, im, fm)
+    print(f"(BIM) Model is: {model}")
+    model = normalize_tree(apply_IM_with_rules(bpic_log, rules))
     print(f"Model is: {model}")
+
     # gviz = pm4py.visualization.petri_net.visualizer.apply(net, im, fm)
     # pm4py.visualization.petri_net.visualizer.view(gviz)
+    from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 
-    fitness = pm4py.fitness_token_based_replay(bpic_log, net, im, fm)
+    model = inductive_miner.apply(bpic_log)
+    fitness = pm4py.fitness_alignments(bpic_log, net, im, fm)
     print(f"Fitness: {fitness}")
-    prec = pm4py.precision_token_based_replay(bpic_log, net, im, fm)
+    prec = pm4py.precision_alignments(bpic_log, net, im, fm)
     print(f"Prec: {prec}")
     """
     examples = [
