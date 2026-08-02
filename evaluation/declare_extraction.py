c9ae4361d95b1d912a579d7efc91484da4983856
Anton Antonov <anton.antonov@rwth-aachen.de>
2026-08-02 13:52:26 +0200

    Update parameter

--- evaluation/declare_extraction.py
+++ evaluation/declare_extraction.py
@@ -451,7 +451,7 @@ def evaluate(
                 f"Event log for process {idx} is empty " "after preprocessing."
             )
 
-        max_trace_length = min(max(len(trace) for trace in event_log), 10)
+        max_trace_length = min(max(len(trace) for trace in event_log), 5)
 
         path_all_traces = os.path.join(
             dataset_dir,
